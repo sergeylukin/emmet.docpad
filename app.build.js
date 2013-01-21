ba1c0a3b1a13640d4f2f3ccf3beda5ad38e9d346
Sergey Lukin <contact@sergeylukin.com>
2013-01-21 16:29:00 -0500

    Set up RequireJS modules

--- app.build.js
+++ app.build.js
@@ -1,8 +1,10 @@
 ({ 
-  appDir: "./out", 
-  baseUrl: "./scripts", 
-  dir: "./build", 
-  modules: [
-    { name: "main" }
-  ]
+  // dir is relative to this file (app.build.js)
+  // dir: "./build", 
+  // appDir is relative to this file (app.build.js)
+  // appDir: "./out", 
+  // baseUrl is relative to appDir
+  baseUrl: "./out/scripts", 
+  // mainConfigFile is relative to this file (app.build.js)
+  mainConfigFile: './out/scripts/main.js'
 })
--- src/files/scripts/main.js
+++ src/files/scripts/main.js
@@ -1,9 +1,13 @@
-
-// require.config({
-//   paths: {
-//     'socket.io': 'socket.io/socket.io'
-//   }
-// });
+// Set configuration for optimi
+requirejs.config({
+  optimize: 'uglify',
+  name: 'main',
+  include: 'requireLib',
+  out: 'require.js',
+  paths: {
+    requireLib: 'lib/require'
+  }
+});
 
 require(['modules/person'], function(person) {
   console.log(person.firstname + ' ' + person.lastname);
